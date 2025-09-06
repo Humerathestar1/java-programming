b4a33457efafd5047f4f22b66e53515936aa0401
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:53:36 +0530

    Create conditionaladult.java

---
+++ conditionaladult.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int age= sc.nextInt();
+        if(age>18){
+            System.out.print("you are adult");
+        }
+         else{
+             System.out.print("you are not an adult");
+         }
+            }
+}
