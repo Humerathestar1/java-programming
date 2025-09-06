f2be4d31c42d4dfca333426bfdbda3d21d3dc7f1
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 23:47:06 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class FactorialLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for(int i =0; i<n;i++){
+            System.out.println("*****");
+        }
+    }
+}
