193bc8eced7ab5a2c4811821df0be630aeaddbc6
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 12:09:42 +0530

    Create nestedif.java

---
+++ nestedif.java
@@ -0,0 +1,20 @@
+public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int button= sc.nextInt();
+
+        if(button==1){
+            System.out.print("Hello");
+        }
+        else if(button==2){
+              System.out.print("Namaste");
+
+        }
+         else if(button==3){
+             System.out.print("Assalamualikum");
+         }
+         else{
+              System.out.print("Invalid no");
+
+         }
+            }
+}
