ff0424cbdde031c8a4ad846e3111b32391942e90
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:40:18 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String Name = sc.nextLine();
+        System.out.print("my name is : "+Name);
+    }
+}
