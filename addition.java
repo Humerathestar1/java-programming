b9013d0c61376c0cd8bd638bd875fc3da764f0e7
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:15:45 +0530

    Create addition.java

---
+++ addition.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[]args){
+        int a = 30;
+        int b = 20;
+        int sum = 0;
+        sum = a+b;
+        System.out.println("The addition of two no is : "+(sum));
+    }
+}
