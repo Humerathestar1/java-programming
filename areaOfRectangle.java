b18634fbdb9c85382c1cbb844082deb859bee3c7
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:47:57 +0530

    Create areaOfRectangle.java

---
+++ areaOfRectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int width = sc.nextInt();
+        int area;
+        area = length*width;
+
+        
+        System.out.print("Area of rectangle   is : "+(area));
+    }
+}
