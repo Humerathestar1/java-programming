edc0d93ba772c935e5def7c5a3e064d504b6f131
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 10:58:26 +0530

    Create output.java

---
+++ output.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String[]args){
+        System.out.println("my name is humaira");
+        System.out.println("I am learning java");
+        System.out.print("This is very amazing");
+    }
+}
