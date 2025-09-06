d8f38ee5776473d0da39e6596e7e1946b6522ef3
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:18:22 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[]args){
+        int a = 60;
+        int b = 2;
+        int mul= 0;
+        mul = a*b;
+        System.out.println("The subtraction of two no is : "+(mul));
+    }
+}
