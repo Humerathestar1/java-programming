08608d9fa09900fa9eb74de449fe019e5a6fdb59
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:23:41 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+class Main{
+    public static void main(String[]args){
+        int a = 60;
+        int b = 2;
+        int sum,sub,multi,divi,modulo;
+        sum = a+b;
+        sub = a-b;
+        multi = a*b;
+        divi = a/b;
+        modulo = a%b;
+        System.out.println("The addition of two no is : "+(sum));
+        System.out.println("The subtraction of two no is : "+(sub));
+        System.out.println("The multiplication of two no is : "+(multi));
+        System.out.println("The division of two no is : "+(divi));
+        System.out.println("The modulo of two no is : "+(modulo));
+
+    }
+}
