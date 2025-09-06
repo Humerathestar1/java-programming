68f1105ad8dc48a207fd899d05b9acb64c89f6c6
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-06 11:17:19 +0530

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[]args){
+        int a = 30;
+        int b = 20;
+        int sub= 0;
+        sub = a-b;
+        System.out.println("The subtraction of two no is : "+(sub));
+    }
+}
