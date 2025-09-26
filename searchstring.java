a9a8619387e150a9c8896981bd57b627c16451f7
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-26 14:33:53 +0530

    Create searchstring.java

---
+++ searchstring.java
@@ -0,0 +1,13 @@
+public class SearchString {
+  public static void main(String[] args) {
+String text = "Hello humaira !";
+String word = "humaira";
+     if (text.contains(word)) {
+     System.out.println("The word is found in the text.");
+        } 
+        else 
+        {
+        System.out.println("The word  is NOT found in the text.");
+        }
+}
+}
