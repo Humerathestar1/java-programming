16eff54d3486ad46d90aecad0da65237d6679744
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-28 22:29:33 +0530

    Create Array.print.java

---
+++ Array.print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int[] marks = new int [3];
+        marks[0] = 98;
+        marks[1] = 99;
+        marks[2] = 45;
+        System.out.println("the marks is : "+marks[0]);
+        System.out.println("the marks is : "+marks[1]);
+        System.out.println("the marks is : "+marks[2]);
+
+    }
+}
