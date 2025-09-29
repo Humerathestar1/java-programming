773558bb1441676b13ae0dbeff6f7de381776167
Humerathestar1 <yasheajjjj@gmail.com>
2025-09-29 11:21:24 +0530

    Create PositiveNegativeZero.java

---
+++ PositiveNegativeZero.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class BitwiseOperations {
+  public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the value :");
+ int n = sc.nextInt();
+if(n>0){
+    System.out.print("positive");
+}
+else if(n<0){
+    System.out.print("Negative");
+}
+else{
+   System.out.print("zero");  
+}
+ 
+    }
+}
