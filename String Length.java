b9c1efa46476583ab686405efc56274a27199198
Humerathestar1 <yasheajjjj@gmail.com>
2025-10-02 13:45:25 +0530

    Create String Length.java

---
+++ String Length.java
@@ -0,0 +1,13 @@
+
+
+class Main {
+    public static void main(String[] args) {
+       
+       String firstname = "Humaira";
+       String Lastname = "Khan";
+       String Fullname = firstname + " "+ Lastname;
+        
+        System.out.println("Length of the name is : " +Fullname.length());
+        
+    }
+}
