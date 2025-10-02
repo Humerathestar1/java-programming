88556793b68c092fcd07975b1e037c3a2204d873
Humerathestar1 <yasheajjjj@gmail.com>
2025-10-02 13:44:13 +0530

    Create String Concatenation.java

---
+++ String Concatenation.java
@@ -0,0 +1,13 @@
+
+
+class Main {
+    public static void main(String[] args) {
+       
+       String firstname = "Humaira";
+       String Lastname = "Khan";
+       String Fullname = firstname + " "+ Lastname;
+        
+        System.out.println("Your name is :" +Fullname);
+        
+    }
+}
