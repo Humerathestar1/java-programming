bcf24218ff1b07016f7d01d0284b46b72bb61f8d
Humerathestar1 <yasheajjjj@gmail.com>
2025-10-02 13:49:08 +0530

    Create CharAt.java

---
+++ CharAt.java
@@ -0,0 +1,15 @@
+
+
+class Main {
+    public static void main(String[] args) {
+       
+       String firstname = "Humaira";
+       String Lastname = "Khan";
+       String Fullname = firstname + " "+ Lastname;
+        
+        for(int  i=0; i<=Fullname.length();i++){
+            System.out.println("Length of the name is : " +Fullname.charAt(i));
+        }
+        
+    }
+}
