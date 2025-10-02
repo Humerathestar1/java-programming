f92e8d7b7642d7abee32983ae72f3aa7be56edaa
Humerathestar1 <yasheajjjj@gmail.com>
2025-10-02 13:59:19 +0530

    Create Substring.java

---
+++ Substring.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+       
+       String sentence = "Humairakhan";
+       String name = sentence.substring(4,sentence.length());
+        
+       
+            System.out.println(name);
+     
+        
+    }
+}
